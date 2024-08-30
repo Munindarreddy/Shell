aaab6de9ec083307d3a5f12207ca97edb4323828
Munindar <muni.484@gmail.com>
2024-08-30 20:36:59 +1000

    added

--- Expense/backend.sh
+++ Expense/backend.sh
@@ -12,7 +12,7 @@ npm install
 chmod -R 775 /app
 chown -R expense:expense /app
 dnf install mysql-server -y
-mysql -h <172.31.34.214> -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 172.31.34.214 -uroot -pExpenseApp@1 < /app/schema/backend.sql
 systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
