ce5726e986d9b9be07bb5bc1e74095cad1343e4c
Munindar <muni.484@gmail.com>
2024-09-01 20:29:37 +1000

    added

--- 003-colors.sh
+++ 003-colors.sh
@@ -15,3 +15,19 @@
 #echo -e "\e[COLORCODEm I am printing a color \e[0m"
 
 echo -e "\e[32m This shows green color \e[0m"
+
+echo -e "\e[33m This shows Yellow color \e[0m"
+
+echo -e "\e[35m This shows Magneta color \e[0m"
+
+# () : Paranthesis
+# [] : Square Bracker
+# {} : Flower Bracker
+
+#### How can we add a backGround color ? Here is the syntax 
+# echo -e "\e[43;31m I am printing Text with a backGround Color \e[0m"
+
+echo -e "\e[46;31m I am printing Text with a backGround color \e[0m"
+# Since in bash and shell, there are lot of versions, both of the syntaxes are valid, in case if anyone doesn't work, prefer to use the other one.
+# echo $a 
+# echo ${a}
