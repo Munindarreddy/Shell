807694aa35a0abc92f801f0abf42d9fbffa94a70
Munindar <muni.484@gmail.com>
2024-09-01 19:18:44 +1000

    added

---
+++ Learn-Shell/Expense/001-shebang.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Shebang Notation : This tells the script with which interpretor this script has to be executed.
+# There can only be one interpretor per script and that should always be the first line of the script.
+
+# Apart from first line if you see any line that starts with # that's reffered as a comment.
+# Anything are just for the purpose of humans to make more verbose.
+
+echo Welcome To Bash Scripting
+
+echo Line1
+echo Line2
+echo Line3
---
+++ Learn-Shell/Expense/002-escSecCharacters.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Escape Sequence Characters
+
+# \n : New Line
+# \t : Move the cursor to a tab space
+
+echo Line1
+echo Line2
+
+echo -e "Line3\nLine4
+echo -e "DevOps\twith\tAWS\tTraining
+
+
+# Whenever you're using special characters, always ecclose them in DOUBLE QUOTES
+# "" : Double Quotes
+# '' : Single Quotes
