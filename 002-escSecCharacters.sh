b3dd8f9e4d4f200f1fcb30539b8c75af981f52e6
Munindar <muni.484@gmail.com>
2024-09-01 20:01:45 +1000

    added

--- 002-escSecCharacters.sh
+++ 002-escSecCharacters.sh
@@ -9,7 +9,7 @@ echo Line1
 echo Line2
 
 echo -e "Line3\nLine4
-echo -e "DevOps\twith\tAWS\tTraining
+echo -e "DevOps\twith\tAWS\tTraining"
 
 
 # Whenever you're using special characters, always ecclose them in DOUBLE QUOTES
